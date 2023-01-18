a84cf4629d36f3ae3e252c05375ea79f877975a5
johndev07 <alwynjohn.dev@gmail.com>
2023-01-18 21:33:54 +0530

    backend completed

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+
+//enable body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/openai", require("./routes/openaiRoutes"));
+app.get("/", (req, res) => {
+  console.log("i am at home page here");
+  res.status(200).json({ sucess: true });
+});
+
+app.listen(PORT, () => {
+  console.log("app started");
+});
---
+++ routes/openaiRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const router = express.Router();
+const { generateImage } = require("../controller/openaiController");
+router.post("/generateimage", generateImage);
+module.exports = router;
